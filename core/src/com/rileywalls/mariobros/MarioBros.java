b10eca24dfea1170f1acfb781d28e9a556231052
rileywalls <32911528+rileywalls@users.noreply.github.com>
2017-12-09 18:41:53 -0500

    Test change

--- core/src/com/rileywalls/mariobros/MarioBros.java
+++ core/src/com/rileywalls/mariobros/MarioBros.java
@@ -31,6 +31,7 @@ public class MarioBros extends Game {
 	public static final short ENEMY_HEAD_BIT = 128;
 	public static final short ITEM_BIT = 256;
 	public static final short MARIO_HEAD_BIT = 512;
+	
 
 	public static AssetManager manager;
 
